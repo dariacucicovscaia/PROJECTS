fe4727aa5f92a8f221726d052dd0d31cb9a9689a
dariacucicovscaia <91362775+dariacucicovscaia@users.noreply.github.com>
2022-05-08 12:47:52 +0300

    Add files via upload

--- TaskManager/src/Factory/TaskFactory.java
+++ TaskManager/src/Factory/TaskFactory.java
@@ -1,8 +1,14 @@
 package Factory;
 
+import java.util.UUID;
+
 import Domain.Task;
 
-public class TaskFactory {
+
+public class TaskFactory extends UUIDgenerator {
 	
-	
+	public Task createTask(String firstName, String lastName, String userName) {
+		Task u = new Task(generateUserUUID(), firstName, lastName, userName);
+		return u;
+	}
 }
---
+++ TaskManager/src/Factory/UUIDgenerator.java
@@ -0,0 +1,13 @@
+package Factory;
+
+import java.util.UUID;
+
+public class UUIDgenerator {
+	/**
+	 * generates an uuid 
+	 * @return user identifier
+	 */
+	public UUID generateUserUUID() {
+		return UUID.randomUUID();
+	}
+}
